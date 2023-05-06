ad99c339998f606e517397f4e52029d1216d97c4
Anastassiya <vyacheslavovna.an@mail.ru>
2023-05-06 13:01:33 +0600

    feat: resolve the 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+  if (err) throw err;
+
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err) throw err;
+
+    for (let i=0; i < files.length; i++) {
+      const {base} = path.parse(path.join(__dirname, 'files', files[i]));
+      const fileInner = fs.createReadStream(path.join(__dirname, 'files', files[i]), 'utf8');
+
+      fs.writeFile(
+        path.join(__dirname, 'files-copy', base),
+        '',
+        (err) => {
+          if (err) throw err;
+          fileInner.on('data', data => {
+            fs.appendFile(
+              path.join(__dirname, 'files-copy', base),
+              data.toString(),
+              (err) => {
+                if (err) throw err;
+              }
+            );
+          });
+        }
+      );
+    }
+  })
+})
