3dab392f678a7e51bd9d4010902c0f7de29f1e26
Anastassiya <vyacheslavovna.an@mail.ru>
2023-05-07 01:18:05 +0600

    fix: 04-copy-files do change when folder already exist

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,32 +1,71 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+fs.readdir(__dirname, (err, files) => {
   if (err) throw err;
-
-  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
-    if (err) throw err;
-
-    for (let i=0; i < files.length; i++) {
-      const {base} = path.parse(path.join(__dirname, 'files', files[i]));
-      const fileInner = fs.createReadStream(path.join(__dirname, 'files', files[i]), 'utf8');
-
-      fs.writeFile(
-        path.join(__dirname, 'files-copy', base),
-        '',
-        (err) => {
+  if (files.includes('files-copy')) {
+    fs.rm(path.join(__dirname, 'files-copy'), { recursive:true }, err => { 
+      if(err) throw err;
+      fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+        if (err) throw err;
+    
+        fs.readdir(path.join(__dirname, 'files'), (err, files) => {
           if (err) throw err;
-          fileInner.on('data', data => {
-            fs.appendFile(
+    
+          for (let i=0; i < files.length; i++) {
+            const {base} = path.parse(path.join(__dirname, 'files', files[i]));
+            const fileInner = fs.createReadStream(path.join(__dirname, 'files', files[i]), 'utf8');
+    
+            fs.writeFile(
               path.join(__dirname, 'files-copy', base),
-              data.toString(),
+              '',
               (err) => {
                 if (err) throw err;
+                fileInner.on('data', data => {
+                  fs.appendFile(
+                    path.join(__dirname, 'files-copy', base),
+                    data.toString(),
+                    (err) => {
+                      if (err) throw err;
+                    }
+                  );
+                });
               }
             );
-          });
+          }
+        })
+      })    
+    });
+
+  } else {
+    fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+      if (err) throw err;
+  
+      fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+        if (err) throw err;
+  
+        for (let i=0; i < files.length; i++) {
+          const {base} = path.parse(path.join(__dirname, 'files', files[i]));
+          const fileInner = fs.createReadStream(path.join(__dirname, 'files', files[i]), 'utf8');
+  
+          fs.writeFile(
+            path.join(__dirname, 'files-copy', base),
+            '',
+            (err) => {
+              if (err) throw err;
+              fileInner.on('data', data => {
+                fs.appendFile(
+                  path.join(__dirname, 'files-copy', base),
+                  data.toString(),
+                  (err) => {
+                    if (err) throw err;
+                  }
+                );
+              });
+            }
+          );
         }
-      );
-    }
-  })
+      })
+    })  
+  }
 })
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.writeFile(
+  path.join(__dirname, 'project-dist', 'bundle.css'),
+  '',
+  err => {
+    if (err) throw err;
+    fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
+      if (err) throw err;
+    
+      for (let i=0; i < files.length; i++) {
+        const {ext} = path.parse(path.join(__dirname, 'styles', files[i]));
+        if (ext === '.css') {
+          
+        }
+        const fileInner = fs.createReadStream(path.join(__dirname, 'files', files[i]), 'utf8');
+  
+        fs.writeFile(
+          path.join(__dirname, 'files-copy', base),
+          '',
+          (err) => {
+            if (err) throw err;
+            fileInner.on('data', data => {
+              fs.appendFile(
+                path.join(__dirname, 'files-copy', base),
+                data.toString(),
+                (err) => {
+                  if (err) throw err;
+                }
+              );
+            });
+          }
+        );
+      }
+    })    
+  }
+)
+
