330598250747a6792243de7c6cf29abe9c2e95d9
Anastassiya <vyacheslavovna.an@mail.ru>
2023-05-07 03:12:14 +0600

    feat: resolve the 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,39 +1,73 @@
 const path = require('path');
 const fs = require('fs');
 
-fs.writeFile(
-  path.join(__dirname, 'project-dist', 'bundle.css'),
-  '',
-  err => {
-    if (err) throw err;
-    fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
+fs.readdir(path.join(__dirname, 'project-dist'), (err, distfiles) => {
+  if (err) throw err;
+  if (distfiles.includes('bundle.css')) {
+    fs.rm(path.join(__dirname, 'project-dist', 'bundle.css'), err => {
       if (err) throw err;
-    
-      for (let i=0; i < files.length; i++) {
-        const {ext} = path.parse(path.join(__dirname, 'styles', files[i]));
-        if (ext === '.css') {
-          
-        }
-        const fileInner = fs.createReadStream(path.join(__dirname, 'files', files[i]), 'utf8');
-  
-        fs.writeFile(
-          path.join(__dirname, 'files-copy', base),
-          '',
-          (err) => {
+      fs.writeFile(
+        path.join(__dirname, 'project-dist', 'bundle.css'),
+        '',
+        err => {
+          if (err) throw err;
+          fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
             if (err) throw err;
-            fileInner.on('data', data => {
-              fs.appendFile(
+          
+            for (let i=0; i < files.length; i++) {
+              const {ext} = path.parse(path.join(__dirname, 'styles', files[i]));
+              if (ext === '.css') {
+                
+              }
+              const fileInner = fs.createReadStream(path.join(__dirname, 'files', files[i]), 'utf8');
+        
+              fs.writeFile(
                 path.join(__dirname, 'files-copy', base),
-                data.toString(),
+                '',
                 (err) => {
                   if (err) throw err;
+                  fileInner.on('data', data => {
+                    fs.appendFile(
+                      path.join(__dirname, 'files-copy', base),
+                      data.toString(),
+                      (err) => {
+                        if (err) throw err;
+                      }
+                    );
+                  });
                 }
               );
-            });
+            }
+          })    
+        }
+      )      
+    })
+  } else {
+    fs.writeFile(
+      path.join(__dirname, 'project-dist', 'bundle.css'),
+      '',
+      err => {
+        if (err) throw err;
+        fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
+          if (err) throw err;
+          for (let i=0; i < files.length; i++) {
+            const {ext} = path.parse(path.join(__dirname, 'styles', files[i]));
+            if (ext === '.css') {
+              const fileInner = fs.createReadStream(path.join(__dirname, 'styles', files[i]), 'utf8');
+              fileInner.on('data', data => {
+                fs.appendFile(
+                  path.join(__dirname, 'project-dist', 'bundle.css'),
+                  data,
+                  (err) => {
+                    if (err) throw err;
+                  }
+                )
+              });  
+            }
           }
-        );
+        })    
       }
-    })    
+    )    
   }
-)
+})
 
